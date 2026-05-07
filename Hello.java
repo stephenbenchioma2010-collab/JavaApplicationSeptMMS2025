648abbebc6ea4fc9471c1a239c12d7da917b949c
Stephen <stephenbenchioma2010@gmail.com>
2026-05-07 08:12:55 -0700

    ReadMe.File

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, I am learning java");
+		System.out.print("Oh wow, learning is fun. ");
+		System.out.print("Hurray, I am happy. ");
+		System.out.printf("Java was created in %d%n",1992);
+		System.out.printf("Java was made free for %c%.2f ",'$',0.0000);
+		System.out.printf("Do you love Java%c%b ",'?',true);
+		System.out.printf("%s","Yes ,Learning Java is fun");
+	}
+}
